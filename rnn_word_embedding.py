0b189fd27fe2a4fc8ec21c5f06d03e43c63b82a7
cld2005 <cld2005@live.cn>
2017-11-26 19:37:04 -0500

    push

---
+++ rnn_word_embedding.py
@@ -0,0 +1,49 @@
+# LSTM with dropout for sequence classification in the IMDB dataset
+import numpy as np
+from keras.datasets import imdb
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers.embeddings import Embedding
+from keras.preprocessing import sequence
+import os
+
+# fix random seed for reproducibility
+np.random.seed(7)
+BASE_DIR = '../'
+GLOVE_DIR = os.path.join(BASE_DIR, 'glove.6B')
+TEXT_DATA_DIR = os.path.join(BASE_DIR, '20_newsgroup')
+
+# load the dataset but only keep the top n words, zero the rest
+
+embeddings_index = {}
+f = open(os.path.join(GLOVE_DIR, 'glove.6B.100d.txt'))
+for line in f:
+    values = line.split()
+    word = values[0]
+    coefs = np.asarray(values[1:], dtype='float32')
+    embeddings_index[word] = coefs
+f.close()
+
+print('Found %s word vectors.' % len(embeddings_index))
+top_words = 5000
+(X_train, y_train), (X_test, y_test) = imdb.load_data(num_words=top_words)
+# truncate and pad input sequences
+MAX_SEQUENCE_LENGTH = 1000
+X_train = sequence.pad_sequences(X_train, maxlen=MAX_SEQUENCE_LENGTH)
+print ('X_train shape',X_train.shape,'\n')
+print (X_train)
+X_test = sequence.pad_sequences(X_test, maxlen=MAX_SEQUENCE_LENGTH)
+# create the model
+EMBEDDING_DIM = 100
+
+model = Sequential()
+model.add(Embedding(top_words, EMBEDDING_DIM, input_length=MAX_SEQUENCE_LENGTH))
+model.add(LSTM(100, dropout=0.2, recurrent_dropout=0.2))
+model.add(Dense(20, activation='sigmoid'))
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
+print(model.summary())
+model.fit(X_train, y_train, epochs=3, batch_size=64)
+# Final evaluation of the model
+scores = model.evaluate(X_test, y_test, verbose=0)
+print("Accuracy: %.2f%%" % (scores[1]*100))
